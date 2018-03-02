aebc818f10bbc9863f2c0248d3cf40d13ebb3f63
MPreston <michael_n_preston@outlook.com>
2018-03-02 00:46:02 -0600

    Updated tests

--- DynamicXmlTests/BreakMeTests.cs
+++ DynamicXmlTests/BreakMeTests.cs
@@ -4,11 +4,12 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace DynamicXmlTests
 {
     [TestClass]
-    public class UnitTest1
+    public class BreakMeTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void HappyPathTest()
         {
+            //all goes well 
         }
     }
 }
