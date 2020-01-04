3f49778e91aa09d591bd33842d9d02b55f979549
mikepreston17 <michael.n.preston@gmail.com>
2020-01-03 20:52:28 -0600

    Removed mispelled directory & csproj

--- Paresly.Test/Extractables/Car.cs
+++
@@ -1,9 +0,0 @@
-namespace Parsely.Test.Extractables
-{
-    public class Car
-    {
-        public string Name { get; set; }
-        public string Make { get; set; }
-        public int Year { get; set; }
-    }
-}
--- Paresly.Test/Extractables/Person.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Parsely.Test.Extractables
-{
-    public class Person
-    {
-        public string Name { get; set; }
-    }
-}
