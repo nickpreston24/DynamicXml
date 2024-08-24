b249fed5e66b1bd4d4ca9554b27ead6fa8f304c1
mikepreston17 <michael.n.preston@gmail.com>
2024-08-23 21:35:50 -0500

    removed obj/"

--- Parsely/DynamicXml/Classes/PocoExtractor.cs
+++ Parsely/DynamicXml/Classes/PocoExtractor.cs
@@ -5,7 +5,6 @@ namespace Parsely
 {
     public class PocoExtractor<T> : IExtract<T> where T : class
     {
-        //TODO: make this return a Maybe<T>
         public T Extract(string xml) => XmlStreamer.StreamInstances<T>(xml).Single();
     }
 }
